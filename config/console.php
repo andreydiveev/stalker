c9d0e5e154a4d24f86537a45eba23dc41035662b
Hell <hell2007xxx@gmail.com>
2012-09-22 07:50:32 -0700

    modified custom config file, modified console config file, modified yiic.php, added migration create_table_user

--- config/console.php
+++ config/console.php
@@ -11,9 +11,9 @@ $config = array(
 
 	// application components
 	'components'=>array(
-		'db'=>array(
+		/*'db'=>array(
 			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
-		),
+		),*/
 		// uncomment the following to use a MySQL database
 		/*
 		'db'=>array(
