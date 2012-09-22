bbe1a88147b973889af48f2fc59b87f95cfed97a
Andrey Diveev <andrey.diveev@gmail.com>
2012-09-22 18:30:51 +0400

    Modified yiic.php for custom config file

--- yiic.php
+++ yiic.php
@@ -3,5 +3,6 @@
 // change the following paths if necessary
 $yiic=dirname(__FILE__).'/../../../../../usr/local/lib/yii/framework/yiic.php';
 $config=dirname(__FILE__).'/config/console.php';
+define('YII_ENVIRONMENT','diveev');
 
 require_once($yiic);
