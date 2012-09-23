3d33fcea3806bd96aab704f6beaf7d4d002c11ca
Andrey Diveev <flat-sea@ya.ru>
2012-09-23 16:28:53 +0400

    Removed yiic.php from trunk

--- yiic.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// change the following paths if necessary
-$yiic=dirname(__FILE__).'/../../../../../usr/local/lib/yii/framework/yiic.php';
-$config=dirname(__FILE__).'/config/console.php';
-define('YII_ENVIRONMENT','varzer');
-
-require_once($yiic);
