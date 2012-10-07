c185cf15e2b4c72f809dc46bbac3e730bb50a274
Andrey Diveev <andrey.diveev@gmail.com>
2012-10-07 09:37:32 +0400

    merge

---
+++ yiic.php
@@ -0,0 +1,8 @@
+<?php
+
+// change the following paths if necessary
+$yiic=dirname(__FILE__).'/../../../../../usr/local/lib/yii/framework/yiic.php';
+$config=dirname(__FILE__).'/config/console.php';
+define('YII_ENVIRONMENT','dieev');
+
+require_once($yiic);
